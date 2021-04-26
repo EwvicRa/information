648993055db2994564e8d10c30fb1ec11728c71a
EwvicRa <fengbu19920307@126.com>
2021-04-26 13:57:06 +0800

    抽取APP的创建逻辑到业务逻辑的模块里面

--- manage.py
+++ manage.py
@@ -1,28 +1,9 @@
-from flask import Flask, session
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import CSRFProtect
-from redis import StrictRedis
-# 扩展库里面的session  可以用来指定session保存的位置，点击扩展源码可查看
-from flask_session import Session
+from flask import  session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from config import *
+from info import app,db
 
 
-app = Flask(__name__)
-
-# 加载配置
-app.config.from_object(Config)
-
-# 初始化数据库
-db = SQLAlchemy(app)
-
-# 初始化Redis 存储对象
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-# 开启CSRF保护 , 只做服务器保护功能 还得自己写cookie
-CSRFProtect(app)
-# 设置session保存指定位置
-Session(app)
 
 # 使用flask—script的命令格式操作flask，先连接一下
 manager = Manager(app)
